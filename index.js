af7c4371b4c007cb327f3bbb1899d68b2c469487
SRbot7 <ing.saul.romero2@gmail.com>
2026-05-31 21:02:01 -0600

    Agrega descarga automática de evidencias

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
+const mime = require('mime-types');
+const path = require('path');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -28,8 +30,6 @@ client.on('message_create', async message => {
 
     const chat = await message.getChat();
 
-    console.log('GRUPO DETECTADO:', chat.name);
-    // SOLO grupos
     if (chat.isGroup && chat.name === 'BITACORA - MTTO - SHP1') {
 
         const fecha = new Date().toISOString();
@@ -49,8 +49,34 @@ ${message.body}
         fs.appendFileSync('bitacora.txt', texto);
 
         console.log('Mensaje guardado');
+
+        // DESCARGAR EVIDENCIAS
+        if (message.hasMedia) {
+
+            const media = await message.downloadMedia();
+
+            if (media) {
+
+                const fechaCarpeta = new Date().toISOString().split('T')[0];
+
+                const carpeta = `evidencias/${fechaCarpeta}`;
+
+                if (!fs.existsSync(carpeta)) {
+                    fs.mkdirSync(carpeta, { recursive: true });
+                }
+
+                const extension = mime.extension(media.mimetype);
+
+                const nombreArchivo = `${Date.now()}.${extension}`;
+
+                const rutaArchivo = path.join(carpeta, nombreArchivo);
+
+                fs.writeFileSync(rutaArchivo, media.data, 'base64');
+
+                console.log('📸 Evidencia guardada:', rutaArchivo);
+            }
+        }
     }
 });
 
 client.initialize();
-
