368e089493a9fc2525efac6f440a511121925ebc
SRbot7 <ing.saul.romero2@gmail.com>
2026-06-14 21:53:57 -0600

    Migra configuracion a variables de entorno

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'admin',
-    host: 'localhost',
-    database: 'operaciones',
-    password: 'admin123',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
 });
 
 module.exports = pool;
